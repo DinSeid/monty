42835ffecff6237073c1308d560cdd808fe180da
dinseid <dinzn@yahoo.com>
2023-03-23 00:04:55 -0700

    compiled

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -7,11 +8,10 @@
 */
 int main(int argc, char *argv[])
 {
-bus_t bus = {NULL, NULL, NULL, 0};
 	char *content;
 	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
+/**	size_t size = 0;*/
+	size_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
@@ -30,7 +30,7 @@ bus_t bus = {NULL, NULL, NULL, 0};
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = getline(&content, &size, file);
+/**		read_line = getline(&content, &size, file);*/
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
